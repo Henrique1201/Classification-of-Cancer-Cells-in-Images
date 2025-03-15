a12077fc649951668203c988a06c96fa88c3f74f
henrique1201 <luizhenriqueteixe@outlook.com>
2025-03-14 22:42:32 -0300

    add function to create dataframe from image directories

---
+++ functions/processing.py
@@ -0,0 +1,19 @@
+import os
+import pandas as pd
+
+def create_dataframe(DATA_PATH):
+    image_paths = []
+    labels = []
+
+    for patient_id in os.listdir(DATA_PATH):
+        patient_folder = os.path.join(DATA_PATH, patient_id)
+        if os.path.isdir(patient_folder):
+            for label in ['0', '1']:
+                label_folder = os.path.join(patient_folder, label)
+                if os.path.isdir(label_folder):
+                    for img_name in os.listdir(label_folder):
+                        image_paths.append(os.path.join(label_folder, img_name))
+                        labels.append(str(label))
+
+    df = pd.DataFrame({'image_path': image_paths, 'label': labels})
+    return df
