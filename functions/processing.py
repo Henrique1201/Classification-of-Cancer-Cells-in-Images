d492148e500f892b0f5df2e4d0026d7378f1cebd
henrique1201 <luizhenriqueteixe@outlook.com>
2025-05-19 11:08:48 -0300

    .

--- functions/processing.py
+++ functions/processing.py
@@ -6,14 +6,13 @@ def create_dataframe(DATA_PATH):
     labels = []
 
     for patient_id in os.listdir(DATA_PATH):
-        patient_folder = os.path.join(DATA_PATH, patient_id)
-        if os.path.isdir(patient_folder):
-            for label in ['0', '1']:
-                label_folder = os.path.join(patient_folder, label)
-                if os.path.isdir(label_folder):
-                    for img_name in os.listdir(label_folder):
-                        image_paths.append(os.path.join(label_folder, img_name))
-                        labels.append(str(label))
+        patient_folder = os.path.join(DATA_PATH)
+        for label in ['0', '1']:
+            label_folder = os.path.join(patient_folder, label)
+            if os.path.isdir(label_folder):
+                for img_name in os.listdir(label_folder):
+                    image_paths.append(os.path.join(label_folder, img_name))
+                    labels.append(str(label))
 
     df = pd.DataFrame({'image_path': image_paths, 'label': labels})
     return df
